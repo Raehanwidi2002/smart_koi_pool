a3a4f0a2681acc3a7c1a52911ed81cad47201ec9
Raehanwidi2002 <rehanwidisugiarto2002@gmail.com>
2022-12-08 19:25:38 +0700

    Gagasl 1

---
+++ lib/signup.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulBuilder{
+  SignupPage({required super.builder});
+
+
+  @override
+  _SignupPageState createState() =>  _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage>{
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold();
+  }
+}
